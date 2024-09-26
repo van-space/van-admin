7b72ecf0ecb882e8a9dc68878d27d1d4ca4e4915
1604549268@qq.com <1604549268@qq.com>
2024-09-26 16:43:00 +0800

    fix: 完善登录跳转逻辑

--- src/router/loader.ts
+++ src/router/loader.ts
@@ -3,13 +3,37 @@ import type { LoaderFunctionArgs } from 'react-router-dom'
 
 import { API_URL, GATEWAY_URL } from '~/constants/env'
 import { SESSION_WITH_LOGIN } from '~/constants/keys'
+import { getTokenIsUpstream } from '~/store/slice/user.slice'
 import { removeToken, setToken } from '~/utils/auth'
 import { RESTManager } from '~/utils/rest'
 
 let lastCheckedLogAt = 0
 let loginWithTokenOnce = false
+
 export async function protectedLoader({ request }: LoaderFunctionArgs) {
   const path = new URL(request.url).pathname
+  if (path === '/setup-api') {
+    return null
+  }
+  if (!API_URL || !GATEWAY_URL) {
+    console.log(
+      'missing api url or gateway url',
+      API_URL,
+      GATEWAY_URL,
+      ', redirect to /setup-api',
+    )
+    return redirect('/setup-api')
+  }
+  // guard for setup route
+
+  if (path === '/setup') {
+    // TODO const isInit = await checkIsInit()
+    const isInit = false
+    console.log('[isInit]', isInit)
+    if (isInit) {
+      return redirect('/')
+    }
+  }
   if (path.startsWith('/dev')) {
     return null
   }
@@ -22,23 +46,19 @@ export async function protectedLoader({ request }: LoaderFunctionArgs) {
   const { ok } = await RESTManager.api('master')('check_logged').get<{
     ok: number
   }>()
-  lastCheckedLogAt = now
   if (!ok) {
     const params = new URLSearchParams()
     params.set('from', new URL(request.url).pathname)
     return redirect(`/login?${params.toString()}`)
   }
+  lastCheckedLogAt = now
 
   // TODO 这里初始化ws
 
   const sessionWithLogin = sessionStorage.getItem(SESSION_WITH_LOGIN)
   if (sessionWithLogin) return null
   // login with token only
-  if (
-    loginWithTokenOnce
-    //  ||
-    // getTokenIsUpstream()
-  ) {
+  if (loginWithTokenOnce || getTokenIsUpstream()) {
     return null
   }
   await RESTManager.api.master.login
